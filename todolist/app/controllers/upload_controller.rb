d364ab128650ec39c4ec86844c7f34a242181dc3
ChenKan <ckbsc@126.com>
2013-07-28 20:09:53 +0800

    [dev]实现将上传的xml文件解析为json，并存储在Mongo中 	modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/controllers/upload_controller.rb 	new file:   public/test-xml/test.xml

--- todolist/app/controllers/upload_controller.rb
+++ todolist/app/controllers/upload_controller.rb
@@ -1,9 +1,29 @@
+require 'crack'
+require "mongo"
+
 class UploadController < ApplicationController
   def upload
-    require 'fileutils'
+    #require 'fileutils'
     tmp = params[:mytest][:myfile]
-    file = File.join("public", tmp.original_filename)
-    FileUtils.cp tmp.path, file
+    #print tmp.class
+    #file = File.join("public", tmp.original_filename)
+    #FileUtils.cp tmp.path, file
+
+    file = File.open(tmp.path, "rb")
+    contents = file.read
+    file.close
+    print contents
+
+    #contents = "<a><b>123</b></a>"
+    myXML = Crack::XML.parse(contents)
+    myJSON = myXML.to_hash
+    print myJSON
+
+    connection = Mongo::Connection.new("localhost", 27017)
+    db = connection.db("todo")
+    coll = db.collection("xmls")
+    coll.save(myJSON)
+    #Mongo shell中的嵌套查询语句 - db.xmls.find({ "breakfast_menu.food.price" : "$4.50" })
 
     #redirect_to index
     respond_to do |format|
