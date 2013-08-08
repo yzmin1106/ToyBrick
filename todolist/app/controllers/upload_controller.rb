872defd512d455b7d418f439166d5c35532c04e9
ChenKan <ckbsc@126.com>
2013-08-08 19:32:23 +0800

    [dev]remove mongodb stuff

--- todolist/app/controllers/upload_controller.rb
+++ todolist/app/controllers/upload_controller.rb
@@ -3,22 +3,10 @@ require "mongo"
 
 class UploadController < ApplicationController
   def upload
+    require 'fileutils'
     tmp_file = params[:upload][:file]    #上传文件已经自动存储在"/var/folders/x/y/z/"下面了
-    #puts tmp_file.path
-    file = File.open(tmp_file.path, "rb")
-    contents = file.read
-    file.close
-
-    #contents = "<a><b>123</b></a>"
-    xml = Crack::XML.parse(contents)
-    json = xml.to_hash
-
-    #参见initializers/mongo.rb
-    connection = Mongo::Connection.new("localhost", 27017)
-    db = connection.db("todo")
-    coll = db.collection("xmls")
-    coll.save(json)
-    #Mongo shell中的嵌套查询语句 - db.xmls.find({ "breakfast_menu.food.price" : "$4.50" })
+    file = File.join("public", tmp_file.original_filename)
+    FileUtils.cp tmp_file.path, file
 
     #redirect_to index
     respond_to do |format|
